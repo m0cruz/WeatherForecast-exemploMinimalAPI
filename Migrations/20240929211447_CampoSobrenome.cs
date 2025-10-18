fe8713d8963a537de309a125293caa9fbba7155f
m0cruz <marlonjc44@gmail.com>
2025-10-17 21:10:13 -0300

    PROJETO FINALIZADO!

---
+++ Migrations/20240929211447_CampoSobrenome.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ModuloAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class CampoSobrenome : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Contatos",
+                table: "Contatos");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SobrenomeDoContato",
+                table: "Contatos",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Contatos",
+                table: "Contatos",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Contatos",
+                table: "Contatos");
+
+            migrationBuilder.DropColumn(
+                name: "SobrenomeDoContato",
+                table: "Contatos");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Contat",
+                table: "Contatos",
+                column: "Id");
+        }
+    }
+}
